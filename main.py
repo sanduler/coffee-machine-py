91c4776853dae9b7529567711e34035d9d2b24b6
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-01-26 12:49:52 -0800

    added the date

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Ruben Sanduleac
 # Date: January 26th, 2022
 # Description: This program simulates a coffee machine where the user
-#              places a specific amount and selects a drink.
+#              places a specific amount and selects a drink. If the amount is
+#              is over the needed amount the sum is refunded.
